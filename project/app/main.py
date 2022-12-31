cc1adbbee0043aeca47eea20cc6b37c0c861a04c
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-12-31 13:52:52 +0100

    ✨ Add database

--- project/app/main.py
+++ project/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
 from app.api import router as api_router
 from app.core.config import get_settings
@@ -39,6 +40,31 @@ def startup_event():
     Base.metadata.create_all(bind=engine)
 
 
+@app.on_event("startup")
+def startup_event():
+    Base.metadata.create_all(bind=engine)
+
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+@app.post("/items/")
+def create_item(name: str, price: int, session: Session = Depends(get_session)):
+    """
+    Create an item.
+
+    \f
+    Args:
+        name (str): The name of the item.
+        price (int): The price of the item.
+        session (Session): The database session.
+
+    Returns:
+        dict: The item.
+    """
+    item = Item(name=name, price=price)
+    session.add(item)
+    session.commit()
+    return item.dict()
