aa02a8eef2d015dd1ace9e79d2caad540a92ebd3
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-12-31 17:24:14 +0100

    🛂 Add authorization setup

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
-from app.main import app
 from fastapi.testclient import TestClient
+
+from app.main import app
 
 
 @pytest.fixture(scope="function")
 def client():
     with TestClient(app) as _client:
+        _client.headers.update({"X-API-Key": "password"})
         yield _client
