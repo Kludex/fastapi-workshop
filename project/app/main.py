248bd9761b15db1cacf545b00d258fa3a23bba7b
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-12-31 16:50:44 +0100

    🎨 Improve project structure

--- project/app/main.py
+++ project/app/main.py
@@ -1,5 +1,4 @@
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
 from app.api import router as api_router
 from app.core.config import get_settings
@@ -38,23 +37,3 @@ def startup_event():
 @app.get("/")
 def home():
     return {"Hello": "World"}
-
-
-@app.post("/items/")
-def create_item(name: str, price: int, session: Session = Depends(get_session)):
-    """
-    Create an item.
-
-    \f
-    Args:
-        name (str): The name of the item.
-        price (int): The price of the item.
-        session (Session): The database session.
-
-    Returns:
-        dict: The item.
-    """
-    item = Item(name=name, price=price)
-    session.add(item)
-    session.commit()
-    return item.dict()
